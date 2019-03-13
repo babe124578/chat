4fe3507e4ab7b12dcd439a29ab167820b6996620
babe124578 <natthapong41@gmail.com>
2019-03-13 12:46:15 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { NavLink } from "react-router-dom";
+
+class App extends Component {
+	render() {
+		return (
+				<div className='name'>
+					<div className='container'>
+						<h1>Please Enter Your Name</h1>
+					</div>
+				</div>
+		);
+	}
+}
+
+export default App;
