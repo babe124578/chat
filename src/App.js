a1c2ac58aba44b98d470c56f70cbe43b2c632ddc
Natthapong <31311467+babe124578@users.noreply.github.com>
2019-03-16 06:04:05 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,35 +6,52 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ChatRoom from './Component/ChatRoom';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+			isClick: false
+		};
+	}
+	
 	render() {
 		return (
-			<Router>
-                <switch>
-                <Route path="/" exact render={() => {
-                    return (
-						<div className='name'>
-							<div className='container'>
-								<h1>Enter Your Name</h1>
-								<br/>
-								<form className="form-group">
-									<input type="text" class="form-control" placeholder="Enter name here ..." id="nameField" required validationmessage="Please enter your name in this field."></input>
-									<br/><br/><br/>
-									<Route>
-									<div>
-										<NavLink to="/ChatRoom">
-											<button class="btn btn-primary" type="submit">Enter</button>
-										</NavLink>
-										
-									</div>
-									</Route>
-								</form>
-							</div>
+			<div className='App-Container'>
+				{
+					this.state.isClick? 
+					<ChatRoom />
+						:
+					<div className='container'>
+						<h1>Enter Your Name</h1>
+						<br/>
+						<input 
+							type="text" 
+							className="form-control" 
+							placeholder="Enter name here ..." 
+							id="nameField" 
+							required
+							onChange={e => {
+								this.setState({ username: this.value });
+							}}
+						></input>
+						<br />
+						<div>
+							<Link to='/ChatRoom'>
+								<button 
+									className="btn btn-primary" 
+									type="submit"
+									onClick={e => {
+										this.setState({ isClick: true });
+										this.setState({ username: 'aaa'});
+										console.log(this.state.isClick);
+										console.log(this.state.username);
+									}}
+								>Enter</button>
+							</Link>
 						</div>
-						);
-					}}/>
-				<Route exact path="/ChatRoom" component={ChatRoom} />
-				</switch>
-			</Router>
+					</div>
+				}
+			</div>
 		);
 	}
 }
