145c06474c624718f994fffd728f1c5e7fa7e11e
Dong <donglovemom@gmail.com>
2019-03-15 17:37:08 +0700

    make clickable sidebar

--- src/Component/chatRoom.js
+++ src/Component/chatRoom.js
@@ -7,26 +7,33 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class chatRoom extends Component {
     render() {
         return (
-            <div class = "container">
-                <div class="chat-container">
-                <div class="message">
-                <div class="datetime">
-                <button type="submit">Join</button>
-                </div>
-                <p>Group1</p>
-                </div>
-                
-                </div>
-
-                <div class="chat-container">
-                <div class="message">
-                <div class="datetime">
-                <button type="submit">Join</button>
-                    
-                </div>
-                <p>Group2</p>
-                </div>
-                </div>
+            <div class = "sideContainer">
+                <NavLink to="/ChangeRoom" style={{color: 'black', textDecoration: 'none'}} activeStyle={{color: 'black', textDecoration: 'none'}}>
+                    <div class="chat-container">
+                    <div class="message">
+                    <div class="datetime">
+                        <NavLink to="/JoinRoom">
+                            <button type="submit">Join</button>
+                        </NavLink>
+                    </div>
+                        <p>Group1</p>
+                    </div>
+                    </div>
+                </NavLink>
+               
+                <NavLink to="/ChangeRoom" style={{color: 'black', textDecoration: 'none'}} activeStyle={{color: 'black', textDecoration: 'none'}}>
+                    <div class="chat-container">
+                    <div class="message">
+                    <div class="datetime">
+                        <NavLink to="/JoinRoom">
+                            <button type="submit">Join</button>
+                        </NavLink>
+                    </div>
+                        <p>Group2</p>
+                    </div>
+                    </div>
+                </NavLink>
+               
 
             </div>
         );
