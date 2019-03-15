07275a55d0a7a288c55d3c27c76806ea30a096a4
Dong <donglovemom@gmail.com>
2019-03-15 15:35:45 +0700

    make side bar with picture date and show current message text

--- src/Component/chatRoom.js
+++ src/Component/chatRoom.js
@@ -3,64 +3,48 @@ import React, { Component } from "react";
 import {NavLink} from 'react-router-dom';
 import './ChatRoom.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import App from '../App';
 
 class chatRoom extends Component {
     render() {
         return (
-        <div>
-            <div id="sidebar">
-                <div class = "toggle-btn">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+            <div class = "container">
+                <div class="chat-container">
+                <div class="message">
+                <img class="avatar" src="https://placeimg.com/50/50/people?1"></img>
+                <div class="datetime">
+                    23/03/2016 20:40
                 </div>
-                <ul>
-                    <p>Group</p>
-                    <p>Group</p>
-                    <p>Group</p>
-                </ul>
-            </div>
+                <p>A message text</p>
+                </div>
+                </div>
 
-            <div id="chatside">
-            <NavLink to="/CreateRoom">
-                <button class="btn btn-primary" type="create">Create</button>
-            </NavLink>
-            
-            <button class="btn btn-primary" type="join">Join</button>
-            
+                <div class="chat-container">
+                <div class="message">
+                <img class="avatar" src="https://placeimg.com/50/50/people?1"></img>
+                <div class="datetime">
+                    23/03/2016 20:40
+                </div>
+                <p>A message text</p>
+                </div>
+                </div>
 
+                <div class="chat-container">
+                <div class="message">
+                <img class="avatar" src="https://placeimg.com/50/50/people?1"></img>
+                <div class="datetime">
+                    23/03/2016 20:40
+                </div>
+                <p>A message text</p>
+                </div>
+                </div>
 
-            <form className="input" onSubmit={(e) => this.submitMessage(e)}>
+                <form className="input" onSubmit={(e) => this.submitMessage(e)}>
                 <input type="text" ref="msg" />
                 <input type="submit" value="Submit" />
             </form>
-            
-            </div>
+            </div>
 
-      
-
-
-            
-
-        </div>
-
-        /*{<Router>
-                <switch>
-                <Route path="/" exact render={() => {
-                    return (
-                        <div className='container'>
-                        <p>test navigate</p>
-                        <NavLink to="../App">
-                            <button class="btn btn-dark" type="button">Enter</button>
-                        </NavLink>
-                        </div>
-                    );
-                }}/>
-                <Route exact path="../App" component={App} />
-                </switch>
-            </Router>
-        }*/
+        
         );
     }
 }
